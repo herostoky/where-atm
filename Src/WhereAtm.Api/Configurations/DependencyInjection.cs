865c9eed1ad718493ff49e6b788ee28a9ad4fb91
Toky <herostoky@gmail.com>
2025-06-26 11:51:54 +0300

    wip project setup

---
+++ Src/WhereAtm.Api/Configurations/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using WhereAtm.Application.Configurations;
+
+namespace WhereAtm.Api.Configurations;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddApplication();
+
+        return services;
+    }
+}
---
+++ Src/WhereAtm.Application/Configurations/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WhereAtm.Application.Configurations;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(m => m.RegisterServicesFromAssemblyContaining(typeof(DependencyInjection)));
+        return services;
+    }
+}
